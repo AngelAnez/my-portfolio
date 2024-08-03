a2d576c6fa82089bb82b145206e4d5eca6aaa3ad
Angel Añez <angdavid2910@gmail.com>
2024-08-03 17:51:10 -0400

    Added new font family, improved SEO and fixed font sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#03DAC6",
+				secondary: "#018786",
+				background: "#121212"
+			}
+		},
 	},
 	plugins: [],
 }
